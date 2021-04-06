5f29e9435986aca38b8f9241807f9b469fd8e5f0
Suelen Almeida <lima.j.diego@gmail.com>
2021-04-06 09:54:15 -0300

    exibindo mensagens de erro

--- frontend/src/app/component/product/product.service.ts
+++ frontend/src/app/component/product/product.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Product } from './product.model';
 
 @Injectable({
@@ -13,34 +15,56 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMsgm(msg: string): void {
+  showMsgm(msg: string, isError: Boolean = true): void {
     this.snackBar.open(msg, 'X', {
       duration: 2000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
+      verticalPosition: 'top',
+      panelClass: isError ['msg-error']
     })
   }
 
   create(product: Product): Observable<Product> { //mandando a requisição http para o back-end
-    return this.http.post<Product>(this.urlBackEnd, product)
-  }
+    return this.http.post<Product>(this.urlBackEnd, product).pipe(
+      map((obj) => obj),
+      catchError( e => this.errorBackend(e))
+    )
+  }  
+
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.urlBackEnd)
+    return this.http.get<Product[]>(this.urlBackEnd).pipe(
+      map((obj) => obj),
+      catchError( e => this.errorBackend(e))
+    )
   }
 
   readById(id: string): Observable<Product> {
     const url = `${this.urlBackEnd}/${id}`
-    return this.http.get<Product>(url)
+    return this.http.get<Product>(url).pipe(
+      map((obj) => obj),
+      catchError( e => this.errorBackend(e))
+    )
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.urlBackEnd}/${product.id}`
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(
+      map((obj) => obj),
+      catchError( e => this.errorBackend(e))
+    )
   }
 
   delete(id: number): Observable<Product> {
     const url = `${this.urlBackEnd}/${id}`
-    return this.http.delete<Product>(url)
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj),
+      catchError( e => this.errorBackend(e))
+    )
+  }
+
+  errorBackend(e: any): Observable<any> {
+    this.showMsgm('Ocorreu um Erro!')
+    return EMPTY
   }
 
 }
